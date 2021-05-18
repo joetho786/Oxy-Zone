505091ee8547e9927e920a62cded1aa9e3237f11
vinuvirat <vinurakav@gmail.com>
2021-05-18 12:02:47 +0530

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { React, useState} from 'react'
+import { React, useState } from 'react'
 import Header from './Header'
+import MapContainer from './Map';
+import Mapbox from './react_map'
+import Maplayers from './map_openlayers'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Header />
+        </Route>
+        <Route path="/map/:loc">
+          <MapContainer />
+        </Route>
+        <Route path="/new">
+          <Mapbox />
+        </Route>
+        <Route path="/mapnew">
+          <Maplayers />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/map_openlayers.js
@@ -0,0 +1,18 @@
+import { ReactBingmaps } from 'react-bingmaps';
+
+const Maplayers = () => {
+    return (
+        <div>
+            <ReactBingmaps
+                bingmapKey="Aue53CqMhv_oforhu3pP6L5EpDqyDwfwptjqrsQdfA_SLBwzPiseBuhSTuAnlB41" >
+                center = {[13.0827, 80.2707]}
+            </ReactBingmaps>
+        </div>
+    )
+}
+
+export default Maplayers
+
+
+
+
