3557d87297298c8367254bb91229da1dd72f6d7f
vinuvirat <vinurakav@gmail.com>
2021-05-18 14:20:03 +0530

    seventh commit

--- src/map_openlayers.js
+++ src/map_openlayers.js
@@ -1,13 +1,13 @@
 import { ReactBingmaps } from 'react-bingmaps';
 
+document.getElementById('root').style['height'] = window.innerHeight + 'px'
+
 const Maplayers = () => {
     return (
-        <div>
-            <ReactBingmaps
-                bingmapKey="Aue53CqMhv_oforhu3pP6L5EpDqyDwfwptjqrsQdfA_SLBwzPiseBuhSTuAnlB41" >
-                center = {[13.0827, 80.2707]}
-            </ReactBingmaps>
-        </div>
+        <ReactBingmaps
+            bingmapKey="Aue53CqMhv_oforhu3pP6L5EpDqyDwfwptjqrsQdfA_SLBwzPiseBuhSTuAnlB41" >
+            center = {[13.0827, 80.2707]}
+        </ReactBingmaps>
     )
 }
 
